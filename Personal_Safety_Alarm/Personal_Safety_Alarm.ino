d0392ad60a18c8d0604b4486200bf2a9a0a36c21
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-12-27 01:09:27 +0800

    Update Personal_Safety_Alarm.ino

--- Personal_Safety_Alarm/Personal_Safety_Alarm.ino
+++ Personal_Safety_Alarm/Personal_Safety_Alarm.ino
@@ -18,6 +18,7 @@ int limitSwitchState;
 int lastLimitSwitchState = HIGH;
 unsigned long lastLimitSwitchDebounceTime = 0;
 int limitSwitchCount = 0;
+int limitSwitchCounter = 0; //counter for when limit switch is pressed
 
 /*debounce delay for buttons*/
 unsigned long debounceDelay = 50;
@@ -27,93 +28,118 @@ unsigned long currentTime;
 unsigned long pressStartTime;
 
 void setup() {
-   Serial.begin(9600);
-   pinMode(streetButtonPin, INPUT_PULLUP);
-   pinMode(disasterButtonPin, INPUT_PULLUP);
+  Serial.begin(9600);
+  pinMode(streetButtonPin, INPUT_PULLUP);
+  pinMode(disasterButtonPin, INPUT_PULLUP);
+  pinMode(limitSwitchPin, INPUT_PULLUP);
 }
 
 void loop() {
-   handleReset();
-   handleStreetButton();
-   handleDisasterButton();
-   handleCondition();
+  handleReset();
+  handleStreetButton();
+  handleDisasterButton();
+  handleCondition();
+  handleLimitSwitch();
 }
 
 void handleCondition() {
-   currentTime = millis();
-   if (currentTime - pressStartTime > 5000) {
-      if (buttonPressCountStreet > 0) {
-         switch (buttonPressCountStreet) {
-         case 1:
-            Serial.println("Fire");
-            break;
-         case 2:
-            Serial.println("Water");
-            break;
-         case 3:
-            Serial.println("Earth");
-            break;
-         }
-         buttonPressCountStreet = 0;
-      } else if (buttonPressCountDisaster > 0) {
-         switch (buttonPressCountDisaster) {
-         case 1:
-            Serial.println("Harrasment");
-            break;
-         case 2:
-            Serial.println("Theft");
-            break;
-         case 3:
-            Serial.println("Abduction");
-            break;
-         }
-         buttonPressCountDisaster = 0;
+  currentTime = millis();
+  if (currentTime - pressStartTime > 5000) {
+    if (buttonPressCountStreet > 0) {
+      switch (buttonPressCountStreet) {
+        case 1:
+          Serial.println("Fire");
+          break;
+        case 2:
+          Serial.println("Water");
+          break;
+        case 3:
+          Serial.println("Earth");
+          break;
       }
-   }
+      buttonPressCountStreet = 0;
+    } else if (buttonPressCountDisaster > 0) {
+      switch (buttonPressCountDisaster) {
+        case 1:
+          Serial.println("Harrasment");
+          break;
+        case 2:
+          Serial.println("Theft");
+          break;
+        case 3:
+          Serial.println("Abduction");
+          break;
+      }
+      buttonPressCountDisaster = 0;
+    }
+  }
 }
 
 void handleStreetButton() {
-   int reading = digitalRead(streetButtonPin);
-   if (reading != lastStreetButtonState) {
-      lastStreetButtonDebounceTime = millis();
-   }
+  int reading = digitalRead(streetButtonPin);
+  if (reading != lastStreetButtonState) {
+    lastStreetButtonDebounceTime = millis();
+  }
 
-   if ((millis() - lastStreetButtonDebounceTime) > debounceDelay) {
-      if (reading != streetButtonState) {
-         streetButtonState = reading;
-         if (streetButtonState == LOW) {
-            pressStartTime = millis();
-            buttonPressCountStreet++;
-            Serial.println(buttonPressCountStreet);
-         }
+  if ((millis() - lastStreetButtonDebounceTime) > debounceDelay) {
+    if (reading != streetButtonState) {
+      streetButtonState = reading;
+      if (streetButtonState == LOW) {
+        pressStartTime = millis();
+        buttonPressCountStreet++;
+        Serial.println(buttonPressCountStreet);
       }
-   }
-   lastStreetButtonState = reading;
+    }
+  }
+  lastStreetButtonState = reading;
 }
 
 void handleDisasterButton() {
-   int reading = digitalRead(disasterButtonPin);
-   if (reading != lastDisasterButtonState) {
-      lastDisasterButtonDebounceTime = millis();
-   }
+  int reading = digitalRead(disasterButtonPin);
+  if (reading != lastDisasterButtonState) {
+    lastDisasterButtonDebounceTime = millis();
+  }
 
-   if ((millis() - lastDisasterButtonDebounceTime) > debounceDelay) {
-      if (reading != disasterButtonState) {
-         disasterButtonState = reading;
-         if (disasterButtonState == LOW) {
-            pressStartTime = millis();
-            buttonPressCountDisaster++;
-            Serial.println(buttonPressCountDisaster);
-         }
+  if ((millis() - lastDisasterButtonDebounceTime) > debounceDelay) {
+    if (reading != disasterButtonState) {
+      disasterButtonState = reading;
+      if (disasterButtonState == LOW) {
+        pressStartTime = millis();
+        buttonPressCountDisaster++;
+        Serial.println(buttonPressCountDisaster);
       }
-   }
-   lastDisasterButtonState = reading;
+    }
+  }
+  lastDisasterButtonState = reading;
+}
+
+void handleLimitSwitch() {
+  int reading = digitalRead(limitSwitchPin);
+  if (reading != lastLimitSwitchState) {
+    lastLimitSwitchDebounceTime = millis();
+  }
+
+  if ((millis() - lastLimitSwitchDebounceTime) > debounceDelay) {
+    if (reading != limitSwitchState) {
+      limitSwitchState = reading;
+      if (limitSwitchState == HIGH) {
+        pressStartTime = millis();
+        Serial.println("Intrusion");
+      }
+      else {
+     
+           limitSwitchCounter++;
+        Serial.println(limitSwitchCounter);
+      }
+    }
+  }
+  lastLimitSwitchState = reading;
 }
 
 void handleReset() {
-   if (buttonPressCountStreet == 4) {
-      buttonPressCountStreet = 1;
-   } else if (buttonPressCountDisaster == 4) {
-      buttonPressCountDisaster = 1;
-   }
+  if (buttonPressCountStreet == 4) {
+    buttonPressCountStreet = 1;
+  } else if (buttonPressCountDisaster == 4) {
+    buttonPressCountDisaster = 1;
+  }
 }
