086ae5b0891cc50f828f69574a034626e0dbdbb4
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-12-28 19:43:02 +0800

    Updated:
    Added GSM Module (Sim900a)
    Added GPS Module (Neo6m)

--- Personal_Safety_Alarm/Personal_Safety_Alarm.ino
+++ Personal_Safety_Alarm/Personal_Safety_Alarm.ino
@@ -1,3 +1,13 @@
+/*GPS Module library*/
+#include <TinyGPSPlus.h>
+TinyGPSPlus gps;
+
+/*Assign Serial1 as gpsModule*/
+#define gpsModule Serial1
+
+/*Assign Serial2 as sim900*/
+#define sim900 Serial2
+
 /*Street button*/
 const int streetButtonPin = 2;
 int streetButtonState;
@@ -27,11 +37,21 @@ unsigned long debounceDelay = 50;
 unsigned long currentTime;
 unsigned long pressStartTime;
 
+String streetDanger = "";
+String disasterDanger = "";
+
+int gsmLocation = 0;
+
+int intrusionCount = 0;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(streetButtonPin, INPUT_PULLUP);
   pinMode(disasterButtonPin, INPUT_PULLUP);
   pinMode(limitSwitchPin, INPUT_PULLUP);
+  gpsModule.begin(9600);
+  sim900.begin(115200);
+  introMessage();
 }
 
 void loop() {
@@ -40,35 +60,67 @@ void loop() {
   handleDisasterButton();
   handleLimitSwitch();
   handleReset();
+
+  while (gpsModule.available() > 0)
+    if (gps.encode(gpsModule.read()) && gsmLocation <= 1)
+    {
+
+      gsmLocation++;
+    }
 }
 
 void streetCase() {
   switch (buttonPressCountStreet) {
     case 1:
       Serial.println("Abduction Happens");
+      streetDanger = "There is an abduction here in my area:";
       break;
     case 2:
       Serial.println("Mugging Happens");
+      streetDanger = "There's a burglary here in my area:";
       break;
     case 3:
       Serial.println("Harrasment Happens");
+      streetDanger = "Someone got harrased in my area:";
       break;
   }
+  sendDangerMessage(streetDanger);
   buttonPressCountStreet = 0;
+  streetDanger = "";
 }
 void disasterCase() {
   switch (buttonPressCountDisaster) {
     case 1:
       Serial.println("Flood and Landslide happens");
+      disasterDanger = "Flood and Landslide here";
       break;
     case 2:
-      Serial.println("Mugging Happens");
+      Serial.println("Earthquake Happens");
+      disasterDanger = "Earthquake here";
       break;
     case 3:
       Serial.println("Fire indicent happens");
+      disasterDanger = "Fire here";
       break;
   }
+  sendDangerMessage(disasterDanger);
   buttonPressCountDisaster = 0;
+  disasterDanger = "";
+
+}
+
+void intrusionCase() {
+  switch (intrusionCount)
+  {
+    case 0:
+      Serial.println("no intrusion");
+      break;
+    default:
+      Serial.println("Intrusion");
+      sendDangerMessage("Intrusion");
+
+  }
+  buttonPressCountStreet = 0;
 }
 
 void handleCondition() {
@@ -78,6 +130,10 @@ void handleCondition() {
       streetCase();
     } else if (buttonPressCountDisaster > 0) {
       disasterCase();
+    }
+    else if (intrusionCount > 0) {
+      Serial.println(intrusionCount);
+      intrusionCase();
     }
   }
 }
@@ -130,7 +186,9 @@ void handleLimitSwitch() {
     if (reading != limitSwitchState) {
       limitSwitchState = reading;
       if (limitSwitchState == HIGH) {
-        Serial.println("Intrusion");
+        //        Serial.println("Intrusion");
+        //        sendDangerMessage("");
+        intrusionCount++;
       }
       else {
 
@@ -146,4 +204,57 @@ void handleReset() {
   } else if (buttonPressCountDisaster == 4) {
     buttonPressCountDisaster = 1;
   }
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+    //    sendCoordinates();
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+  Serial.println("");
+}
+
+void sendDangerMessage(String dangerMessage)
+{
+  Serial2.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  Serial2.println("AT+CMGS=\"+639275156419\"\r"); // Replace x with mobile number
+  delay(1000);
+  Serial2.print("Hi! This is a test message. Don't get alarmed. <3 \n" );
+  Serial2.print(dangerMessage);
+  Serial2.print("Link to my location: \n");
+
+  if (gps.location.isValid())
+  {
+    Serial2.print(gps.location.lat(), 6);
+    Serial2.print(F(","));
+    Serial2.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial2.print(F("INVALID LOCATION"));
+  }
+  delay(100);
+  Serial2.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+void introMessage()
+{
+  Serial2.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  Serial2.println("AT+CMGS=\"+639275156419\"\r"); // Replace x with mobile number
+  delay(1000);
+  Serial2.print("Receiving this text means that the device works!" );
+  delay(100);
+  Serial2.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
 }
